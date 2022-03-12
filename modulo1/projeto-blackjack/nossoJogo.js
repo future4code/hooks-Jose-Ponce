a7a07105d7fc998123eeca55bdd297543fd99b43
zeponce <jgponce@gmail.com>
2022-03-11 21:00:42 -0300

    BlackJack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,43 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+
+// const jogadorCarta1 = comprarCarta()
+// const jogadorCarta2 = comprarCarta()
+// const valorJogador = jogadorCarta1.valor + jogadorCarta2.valor
+// const computadorCarta1 = comprarCarta() 
+// const computadorCarta2 = comprarCarta()
+// const valorComputador = computadorCarta1.valor + computadorCarta2.valor
+// //somar o valor das duas cartas do jogador e somar tambem do computador
+// console.log("Seja bem vindo ao jogo BlackJack!")
+
+// if(confirm("Clique em OK para iniciar uma nova rodada!")){
+//    console.log(`Usuário - Cartas ${jogadorCarta1.texto} ${jogadorCarta2.texto} - pontuação ${valorJogador} `)
+//    console.log(`Computador - Cartas ${computadorCarta1.texto} ${computadorCarta2.texto} - pontuação ${valorComputador} `)
+//    if (valorJogador>valorComputador){
+//       console.log("O Jogador Ganhou")
+//    }
+//    else if(valorJogador<valorComputador){
+//       console.log("O Computador Ganhou")
+//    }
+//    else{
+//       console.log ("Empate!")
+//    }
+// }
+
+
+
+
+// else{
+//    console.log("O Jogo Acabou :(")
+// }

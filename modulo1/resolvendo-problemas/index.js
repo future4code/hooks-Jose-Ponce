064a04bdb1df050d7912a7bc83af45867d581d3c
zeponce <jgponce@gmail.com>
2022-03-15 22:33:32 -0300

    Resolvendo Problemas - JS

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,20 @@
+//Exercícios de fixação do conhecimento
+
+// Faça um resumo sobre quais são os 7 passos fundamentais para a resolução de problemas e como eles podem nos ajudar a solucionar nossos desafios de programação.
+
+/*
+1) Ter atenção no enunciado: Ler com calma e entender o que esta sendo pedido ou qual é o problema.
+
+2) Retirar informações relevantes: resumir o que esta sendo pedido e começar a pensar nas variaveis, chaves estrangeiras, começar a pensar no que é necessário para a resolução ou desenvolvimento.
+
+3) Pensar nas entradas e saídas: tentar imaginar já o que o funcionario vai digitar ou o que o programa necessita. Esse e o momento para começar a pensar na função que poderá ser desenvolvida.
+
+4) Fazer um esquema visual: Começar um desenho (fluxo grama talvez) de como o programa vai funcionar ou como vai ser montado a resolução do problema. Acredito que nessa hora, também pode ser montado um protótipo de portugol para ter uma outra forma de visualização.
+
+5) Criar um código feio: resumindo...fazer um codigo para começar a por na IDE o que você pensou nos passos acimas, o foco é tentar chegar no "resultado" provável, sem ficar pensando se ele esta bem formatado (o script) ou se esta na melhor ordenação de comandos, etc. 
+
+6) Testar - Força-Bruta: Colocar o codigo em cima(5), com a resolução já realizada, na linguagem solicitada e normalmente em português...(dependendo da empresa e de onde voce está)
+
+7) Refatorar: Agora sim colocar a formatação em prática, como a identação, bons nomes para varivaeis, camelCase, boa organização de funções e declaração das variáveis.
+
+*/

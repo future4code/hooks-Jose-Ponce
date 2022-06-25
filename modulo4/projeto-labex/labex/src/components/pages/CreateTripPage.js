e6d60d9aa022aa1746742b9ea3f3d46d8c4b84f2
zeponce <jgponce@gmail.com>
2022-06-24 21:55:48 -0300

    ProjetoLabeX

--- modulo4/projeto-labex/labex/src/components/pages/CreateTripPage.js
+++ modulo4/projeto-labex/labex/src/components/pages/CreateTripPage.js
@@ -1,9 +1,36 @@
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+display:flex;
+justify-content: space-around;
+`
+const TitleContainer = styled.h1`
+text-align: center;
+`
+const TripCreationContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+box-shadow: RGB(0 0 0 / 70%) 2px 5px 5px 2px;
+`
+
 const CreateTripPage = () => {
    
     return (
         
         <>
-            <p>CreateTripPage!!</p>
+            <TitleContainer>Criar Viagem:</TitleContainer>
+            <TripCreationContainer>
+                <p>NOME: VIAGEM JUPITER</p>   
+                <p>ESCOLHA UM PLANETA</p>   
+                <p>DATA</p>   
+                <p>DESCRICAO</p>   
+                <p>DURAÇÃO EM DIAS</p>   
+            </TripCreationContainer>
+            <ButtonContainer>
+                <button>Voltar</button>
+                <button>Criar</button>
+            </ButtonContainer>
         </>
     )
 
--- modulo4/projeto-labex/labex/src/components/pages/TripDetailsPage.js
+++ modulo4/projeto-labex/labex/src/components/pages/TripDetailsPage.js
@@ -1,9 +1,66 @@
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+display:flex;
+justify-content: space-around;
+`
+const TitleContainer = styled.h1`
+text-align: center;
+`
+const TripDetailsContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+const PersonDetailsContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+const AprovedPersonDetailsContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
 const TripDetailsPage = () => {
    
     return (
         
         <>
-            <p>TripDetailsPage!!</p>
+            <TitleContainer>Viagem 1</TitleContainer>
+            
+            <TripDetailsContainer>
+                <p>NOME: VIAGEM 1</p>
+                <p>DESCRIÇÃO: DESCRICAO VIAGEM</p>
+                <p>PLANETA: JUPITER</p>
+                <p>DURACAO: 100 DIAS</p>
+                <p>DATA: 20/12/2020</p>
+            </TripDetailsContainer>
+            
+            <ButtonContainer>
+                <button>Voltar</button>
+            </ButtonContainer>
+            
+            <TitleContainer>Candidatos Pendentes</TitleContainer>
+            <PersonDetailsContainer>
+                <p>NOME: CANDIDATO</p>
+                <p>PROFISSAO: ESTUDANTE</p>
+                <p>IDADE: 68 ANOS</p>
+                <p>PAÍS: BRASIL</p>
+                <p>TEXTO DA CANDIDATURA: QUERO MT VIAJAAAR</p>
+            </PersonDetailsContainer>
+            <ButtonContainer>
+                <button>Aprovar</button>
+                <button>Reprovar</button>
+            </ButtonContainer>
+            <TitleContainer>Candidatos Aprovados</TitleContainer>
+            <AprovedPersonDetailsContainer>
+                <ol>
+                    <li>Fulano</li>
+                    <li>Ciclano</li>
+                </ol>
+            </AprovedPersonDetailsContainer>
         </>
     )
 

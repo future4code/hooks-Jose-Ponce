508947511fc272c569a18c61f3865d945c57e31e
zeponce <jgponce@gmail.com>
2022-07-26 22:45:49 -0300

    Cont-StarWars

--- modulo4/projeto-StarWars/star-wars-app/src/CharacterListPage/CharacterListPage.js
+++ modulo4/projeto-StarWars/star-wars-app/src/CharacterListPage/CharacterListPage.js
@@ -1,47 +1,61 @@
 import styled from "styled-components";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { BASE_URL } from "../constants/url";
 
-const CardList = styled.div`
+const CardContainer = styled.div`
 display: flex;
 flex-direction: column;
 `
 
-const CharacterName = styled.p`
+const CardCharacterList = styled.p`
 border: 1px solid black;
 height: auto;
 width: 30%;
 text-align: center;
+&:hover {
+    background-color:green;
+}
 `
 
 const CharacterListPage = () => {
 
     const [characterList, setCharacterList] = useState([])
 
-    const getCharacters =() =>{
-    axios.get('https://swapi.dev/api/')
+//----------COLOCAR CUSTOMHOOK AQ--------------------------
+    const getCharacters = () =>{
+    axios.get(`${BASE_URL}/people`)
+
     .then((res)=>{
-        setCharacterList(res.data.people)
-        console.log(res)
+        setCharacterList(res.data.results)
+        console.log(res.data.results)
     })
     .catch((err)=>{
         console.log(err)
     })
     }
-    
     useEffect(() => {
         getCharacters()
     
     },[])
+//----------COLOCAR CUSTOMHOOK AQ--------------------------
+// const renderHabilidade = abilities && abilities.map((hab) => {
+//     return (<p>{hab.ability.name.toUpperCase()}</p>)
+
+const characterListRender = characterList.map((character) => {
+    return( 
+        <CardCharacterList>
+        <p>{character.name}</p>
+        </CardCharacterList>
+        )
+})
 
     return(
     <>
         <h1>Lista de Personagens</h1>
-        <CardList>
-            <CharacterName>Luke Skywalker</CharacterName>
-            <CharacterName>C-3PO</CharacterName>
-            <CharacterName>R2-D2</CharacterName>
-        </CardList>
+        <CardContainer>
+        {characterListRender}
+        </CardContainer>
     </>
     )
 }
